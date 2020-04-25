dde442705907ff0b64f7f2905f33f880ce2deed2
syrup3663dev <57992579+syrup3663dev@users.noreply.github.com>
2020-04-25 19:12:35 +0900

    記事モデルの追加と管理者ページへの反映

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=40)
+    content = models.TextField(blank=False, null=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
