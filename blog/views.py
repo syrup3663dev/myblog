241a300c6c048a626b01a3e2e9d86993f174ac3c
syrup3663dev <57992579+syrup3663dev@users.noreply.github.com>
2020-04-26 10:04:38 +0900

    インデックスページをListViewに変更
    - 見た目の調整

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from django.views import generic
+from .models import Article
 
-class IndexView(generic.TemplateView):
+class ArticleListView(generic.ListView):
+    model = Article
+    paginate_by = 5
     template_name = 'index.html'
+
+    def get_context_data(self):
+        context = super().get_context_data()
+        return context
+        
